db594731efae7e59cd9b976cea48dd81030e3b3c
Nozimbek Azlarov <nozim.azlarov101@gmail.com>
2020-08-18 06:23:26 -0600

    It is working

--- catalog/views.py
+++ catalog/views.py
@@ -56,9 +56,10 @@ def check(request):
         checking_form = CheckingForm(request.POST)
         if checking_form.is_valid():
             secret_number = Booking.objects.filter(secret_number=checking_form.cleaned_data['secret_number'])
-            return render(request, 'checking_done.html', {'secret_number': secret_number})
-        else:
-            redirect(movie_play_details.html)
+            if secret_number:
+                return render(request, 'checking_done.html')
+            else:
+                return render(request, 'checking_failed.html')
 
 
 
